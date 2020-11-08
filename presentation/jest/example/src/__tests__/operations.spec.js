965c00acd5e3a13311d5bb7897faa99102d7dc31
matinzd <matinzd619@gmail.com>
2020-11-08 12:11:09 +0330

    update slides and add example

---
+++ presentation/jest/example/src/__tests__/operations.spec.js
@@ -0,0 +1,16 @@
+const { sum } = require("../simple_operations")
+
+/**
+ * 1- Matchers - toEqual - toBe differences
+ * 2- Code coverage
+ * 3- Test suite
+ */
+
+describe('Simple Operations Test Suite', () => {
+
+    it('Should add two numbers properly', () => {
+        expect(sum(1,3)).toEqual(4)
+        expect(sum(1,3)).not.toEqual(6)
+    })
+
+})
---
+++ presentation/jest/example/src/simple_operations.js
@@ -0,0 +1,13 @@
+function sum(a, b) {
+    return a + b;
+}
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// function clone_array(array) {
+//     return [...array]
+// }
+
+module.exports = {sum}
