ebf947a3b87999fbc58e6401de5b20674ff22ade
Matin Zadehdolatabad <zadehdolatabad@gmail.com>
2020-11-08 15:51:24 +0330

    update example

--- presentation/jest/example/src/__tests__/snapshot.spec.js
+++ presentation/jest/example/src/__tests__/snapshot.spec.js
@@ -1,5 +1,7 @@
-const sample = require('../data/sample.json')
+const sample = require("../data/sample.json");
 
-it('sample json should match snapshot', () => {
-    expect(sample).toMatchSnapshot()
-})
+it("sample json should match snapshot", () => {
+  expect(sample).toMatchSnapshot();
+  expect(sample).toHaveProperty("title");
+  expect(sample).toHaveProperty("text");
+});
